d1e28ffd352f5bf2242e89b7829a81bbbffeae0b
Rockyfu <453618045@qq.com>
2022-01-06 18:35:32 +0800

    Update main.go

--- 001_递归/main.go
+++ 001_递归/main.go
@@ -1,1 +1,20 @@
 
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	hanoi(3, "A", "B", "C")
+}
+
+func hanoi(n int64, a string, b string, c string) {
+
+	if n > 0 { //检查是否还存在剩余的盘
+		hanoi(n-1, a, c, b)
+		fmt.Println(" moving from " + a + " to " + c)
+		hanoi(n-1, b, a, c)
+	}
+
+}
