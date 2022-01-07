4a1c270d92c51c7f1b36c7e08681f60d5c7e49e0
Rockyfu <453618045@qq.com>
2022-01-07 20:19:13 +0800

    Update main.go

--- 002_查找-线性查找/main.go
+++ 002_查找-线性查找/main.go
@@ -1,18 +1,17 @@
-
 package main
 
 import "fmt"
 
 func main() {
-	var dataSet [9]int64
+	var dataSet []int64
 	var valueSet int64
-	dataSet = [9]int64{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	valueSet = 9
+	dataSet = []int64{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	valueSet = 10
 	linear_search(dataSet, valueSet)
 
 }
 
-func linear_search(dataSet [9]int64, valueSet int64) {
+func linear_search(dataSet []int64, valueSet int64) {
 	for i := range dataSet {
 		if dataSet[i] == valueSet {
 			fmt.Println(i)
