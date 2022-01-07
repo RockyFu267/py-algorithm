fde612b06ac41da91b90c5219093724ea462a7a8
Rockyfu <453618045@qq.com>
2022-01-07 20:16:17 +0800

    Update main.go

--- 002_查找-线性查找/main.go
+++ 002_查找-线性查找/main.go
@@ -1,1 +1,23 @@
 
+package main
+
+import "fmt"
+
+func main() {
+	var dataSet [9]int64
+	var valueSet int64
+	dataSet = [9]int64{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	valueSet = 9
+	linear_search(dataSet, valueSet)
+
+}
+
+func linear_search(dataSet [9]int64, valueSet int64) {
+	for i := range dataSet {
+		if dataSet[i] == valueSet {
+			fmt.Println(i)
+			return
+		}
+	}
+	fmt.Println("没有啊")
+}
