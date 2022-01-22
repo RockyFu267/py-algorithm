0aadacbc795194a7fd52840960d3d09c2b67662a
RockyFu267 <453618045@qq.com>
2022-01-22 13:49:42 +0800

    test

--- 004_排序-冒泡排序/main.go
+++ 004_排序-冒泡排序/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	var listSet []int64
 	listSet = []int64{9, 8, 7, 1, 2, 0, 3, 4, 6, 5}
@@ -7,8 +9,17 @@ func main() {
 }
 
 func bubble_soft(listSet []int64) {
+	var j int
+	var tmp int64
 	for i := range listSet {
-		println(i)
+		for j <= len(listSet)-i-1 {
+			if listSet[j] > listSet[j+1] {
+				tmp = listSet[j+1]
+				listSet[j+1] = listSet[i]
+				listSet[i] = tmp
+			}
+			j = j + 1
+		}
 	}
-
+	fmt.Println(listSet)
 }
